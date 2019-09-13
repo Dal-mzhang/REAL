8f06232a9860faf64c48cee1d375bd992dc33f04
Miao Zhang <51533859+Dal-mzhang@users.noreply.github.com>
2019-09-13 17:21:45 -0300

    Add files via upload

---
+++ demo_real/REAL/t_dist/t_dist.m
@@ -0,0 +1,42 @@
+close all;
+clc
+clear;
+%
+% plot travel time versus hypocenter distance.
+% Input file: t_dist.dat
+%
+% M. Zhang modified from Hao Guo
+%
+
+
+%% separate P and S t-dist data.
+t_d = load('t_dist.dat');
+n1 = 0; n2 = 0;
+for i = 1:length(t_d(:,1))
+    if t_d(i,3)==1
+        n1 = n1+1;
+        t_d_P(n1,1) = t_d(i,1);
+        t_d_P(n1,2) = t_d(i,2);
+    elseif t_d(i,3)==2
+        n2 = n2+1;
+        t_d_S(n2,1) = t_d(i,1);
+        t_d_S(n2,2) = t_d(i,2);
+    end
+end
+%% plot t-dist curve of P wave
+figure;
+subplot(1,2,1);
+plot(t_d_P(:,2),t_d_P(:,1),'r.');hold on;
+
+axis([0 100 0 30]);
+title('t-dist curve of P');
+xlabel('Hypocenter Distance (km)');ylabel('Travel Time (s)');
+
+%% plot t-dist curve of S wave
+subplot(1,2,2); 
+plot(t_d_S(:,2),t_d_S(:,1),'r.');hold on;
+
+axis([0 100 0 30]);
+title('t-dist curve of S');
+xlabel('Hypocenter Distance (km)');
+print('-depsc2','t_dist','-r300');
