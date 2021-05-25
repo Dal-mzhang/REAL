40c4c120b4bf34b66f32ea175dbcd3ade64fac9d
Miao Zhang <51533859+Dal-mzhang@users.noreply.github.com>
2021-05-25 11:04:30 -0300

    Update runREAL.pl

--- demo_real/REAL/runREAL.pl
+++ demo_real/REAL/runREAL.pl
@@ -10,13 +10,13 @@ $day = "14";
 $D = "$year/$mon/$day/42.75";
 # -R(rx/rh/tdx/tdh/tint[/gap/GCarc0/latref0/lonref0]])
 # station gap constraint is strongly recommended when station coverage is poor
-$R = "0.2/20/0.02/2/3/270/2";
+$R = "0.2/20/0.02/2/3/250/2";
 # -G(trx/trh/tdx/tdh)
 $G = "1.4/20/0.01/1";
 # -V(vp0/vs0/[s_vp0/s_vs0/ielev])
 $V = "6.2/3.3";
-# -S(np0/ns0/nps0/nppp/std0/dtps/nrt[/rsel/ires])
-$S = "5/0/18/1/0.5/0.5/1.3/1.8";
+# -S(np0/ns0/nps0/npsboth0/std0/dtps/nrt[/rsel/ires])
+$S = "3/2/5/2/0.5/0.2/1.5";
 
 $dir = "../Pick/$year$mon$day";
 $station = "../Data/station.dat";
